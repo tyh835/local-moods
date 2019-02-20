5742fd81ab8b82ed9aa5969a89d021e2f30ac932
Tony Han <itony9401@live.com>
2019-02-19 23:29:18 -0800

    removed dotenv

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dotenv from 'dotenv';
 import './index.scss';
 import App from './components/App/App';
-
-dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
