063bf363fc545cb2f598451fed087cc988698440
Tony Han <itony9401@live.com>
2019-02-19 16:08:59 -0800

    formatted files

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
