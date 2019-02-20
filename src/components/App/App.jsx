c9ec5d242ddbb30d1f27107bec6acd0f4702c7aa
Tony Han <itony9401@live.com>
2019-02-19 17:33:31 -0800

    changed site name

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,7 +49,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Moods</h1>
+        <h1>MoodFoods</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList
           businesses={this.state.businesses}
